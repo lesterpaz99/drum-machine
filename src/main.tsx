cda6b6992e0d6ccbbe855d39d9a6edc25034aee7
Obed Paz <lesterpaz99@gmail.com>
2023-08-19 12:44:48 -0600

    Update the react dom render method to 17 version way

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import ReactDOM from 'react-dom';
+// import ReactDOM  from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// react 17
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// react 18
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
