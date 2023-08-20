751f7512a134dcb38fef7b01991ce49947677577
Obed Paz <lesterpaz99@gmail.com>
2023-08-19 19:27:49 -0600

    Wrap the app in the Prime react provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import App from './App.tsx'
-import './index.css'
+import App from './App.tsx';
 import ReactDOM from 'react-dom';
+import { PrimeReactProvider } from 'primereact/api';
+
+//theme
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+//core
+import 'primereact/resources/primereact.min.css';
+import './index.css';
+
 // import ReactDOM  from 'react-dom/client';
 
 // react 17
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<PrimeReactProvider>
+		<App />
+	</PrimeReactProvider>,
+	document.getElementById('root')
+);
 
 // react 18
 // const root = ReactDOM.createRoot(document.getElementById('root'));
