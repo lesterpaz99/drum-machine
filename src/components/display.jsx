9a8f20f106f4f89c66a16b4dc301cef83449861d
Obed Paz <lesterpaz99@gmail.com>
2023-08-20 21:02:19 -0600

    Create the display component

--- src/components/display.jsx
+++
@@ -1,3 +0,0 @@
-export const Display = ({ currentDisplay }) => {
-	return <div>{currentDisplay}</div>;
-};
---
+++ src/components/display.tsx
@@ -0,0 +1,22 @@
+import { useSelector } from 'react-redux';
+import { RootState } from '../context/store';
+
+import { Message } from 'primereact/message';
+
+export const Display = () => {
+	const displayState = useSelector((state: RootState) => state.drumPad.display);
+
+	return (
+		<Message
+			style={{
+				border: 'solid #696cff',
+				borderWidth: '0 6px 0 6px',
+				color: '#696cff',
+				width: '200px',
+			}}
+			className='border-primary w-full justify-content-start'
+			severity='info'
+			content={displayState}
+		/>
+	);
+};
